dde17a1b66ef69efc75958923c62572eef2ea2ba
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2022-09-12 20:40:38 -0300

    adicionada opção de porta 3001 caso falte na variável de ambiente

--- index.js
+++ index.js
@@ -15,6 +15,6 @@ app.use(express.json());
 app.use('/pacients', pacientRoutes);
 app.use('/payments', paymentRoutes);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 

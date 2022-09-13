da0dc703e7707d4d1e7efcdc96398d9ee96307d4
Matheus Figueiredo <matheus.b.figueiredo@outlook.com>
2022-09-13 15:36:05 -0300

    configurando scpript para migrations

--- config/config.js
+++ config/config.js
@@ -17,6 +17,7 @@ module.exports = {
   },
   production: {
     username: process.env.MYSQL_USER,
+    use_env_variable: procces.env.DATABASE_URL,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.TABLE_NAME,
     host: process.env.HOSTNAME,
